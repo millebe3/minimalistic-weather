0d5e8cc5a47fbec188cc4c32f8a6d84d53721a58
Dorothy Carter <dorothy.carter5578@gmail.com>
2016-02-05 16:27:09 -0500

    changed the project to work as an eclipse project. Slightly updated the GetWeather.java and sampleData.xml files.

--- src/GetWeather.java
+++ src/GetWeather.java
@@ -50,6 +50,7 @@ public class GetWeather {
 		base += "&sky=sky"; // cloud cover
 		base += "&wspd=wspd"; // wind speed
 		base += "&wdir=wdir"; // wind direction
+		base += "&rh=rh";
 		
 		weather = new URL(base);
 	}
@@ -72,5 +73,29 @@ public class GetWeather {
 	
 	public String toString() {
 		return weather + "\n" + content;
+	}
+	
+	public static String buildTimestamp(int year, int month, int day, int hour, int minute) {
+		String temp = year + "-";
+		if (month < 10)
+			temp += "0" + month;
+		else
+			temp += month;
+		
+		if (day < 10)
+			temp += "0" + day;
+		else
+			temp += day;
+		
+		temp += "T";
+		
+		if (hour < 10)
+			temp += "0" + hour;
+		else
+			temp += hour;
+		
+		temp += ":00";
+		
+		return temp;
 	}
 }
