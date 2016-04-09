4c06624a5f5b501aec3f2f0fb3b01e0bf6f2f88b
Dorothy Carter <dorothy.carter5578@gmail.com>
2016-04-08 22:53:56 -0400

    added listener for us/metric button, began work on listener for refresh button

--- src/SimpleWeather.java
+++ src/SimpleWeather.java
@@ -8,571 +8,274 @@
 
 //**************************************************************************************************
 
-
-
 import java.awt.*;
-
 import java.awt.event.ActionEvent;
-
 import java.awt.event.ActionListener;
-
-
-
-
-
 import javax.swing.*;
-
 import javax.swing.border.Border;
-
-
+import java.net.*;
+import java.util.InputMismatchException;
+import java.io.IOException;
 
 public class SimpleWeather extends JFrame
-
 {
 
-	
-
 	private static final long serialVersionUID = 1L;
-
+	private boolean isMetric;
 	private JFrame frame;
-
-	private JPanel contents;
-
-	private JPanel panelCenter;
-
-	
-
-	private JPanel panelNorth;
-
-	private JPanel panelSouth;
-
-	
-
+	private JPanel contents, panelCenter, panelNorth, panelSouth;
 	private Border borderContents = BorderFactory.createEmptyBorder(10, 10, 10, 10);
-
 	private Border borderRegion = BorderFactory.createLineBorder(Color.BLACK, 1);
-
 	private Color colorContents = Color.BLUE;
-
 	private Font fontLabels = new Font(Font.DIALOG, Font.BOLD, 16);
-
-	
-
 	private JTextField enterZip;
-
-	
-
 	private JButton getWeather, usMetric;
-
-	
-
-
-
-
-
-	private JLabel highTemp, lowTemp, humid, chanceRain;
-
-	private JLabel high, low, humidity, howMuchRain;
-
-	private JLabel temperature, cloudCover, windSpeed;
-
+	private JLabel highTemp, lowTemp, humid;
+	private JLabel howMuchRain, date;
 	private JLabel temp, cloud, wind, zip, space;
 
 	public SimpleWeather()
-
 	{
-
+		isMetric = false;
 		
-
-		
-
 		// create main panel
-
 		setFonts();
 
 		contents = new JPanel(new GridBagLayout());
 
 		contents.setLayout(new BorderLayout());
-
 		contents.setBorder(borderContents);
-
 		contents.setBackground(colorContents);
 
 		setContentPane(contents);
 
-		
-
-	
-
 		GridBagConstraints gbc = new GridBagConstraints();
-
-		gbc.insets = new Insets(10,10,10,10);
-
-		
+		gbc.insets = new Insets(10, 10, 10, 10);
 
 		
-
-		
-
-		
-
-		
-
 		// create center panel and add labels
-
-		//*******************************************************************
-
 		panelCenter = new JPanel(new GridBagLayout());
-
 		panelCenter.setPreferredSize(new Dimension(800, 300));
-
 		contents.add(panelCenter, BorderLayout.CENTER);
-
 		
-
+		// add these using a method, because we don't need to refer to them again
+		placeLabel(panelCenter, gbc, "Temperature", 0);
+		placeLabel(panelCenter, gbc, "High", 1);
+		placeLabel(panelCenter, gbc, "Low", 2);
+		placeLabel(panelCenter, gbc, "Humidity", 3);
+		placeLabel(panelCenter, gbc, "Chance of Precipitation", 4);
+		placeLabel(panelCenter, gbc, "Cloud Cover", 5);
+		placeLabel(panelCenter, gbc, "Wind Speed", 6);
 		
-
-		
-
-		temperature = new JLabel("Temperature: ");
-
-		gbc.gridx = 0;
-
+		// we do need these ones
+		temp = new JLabel("Hot");
+		gbc.gridx = 1;
 		gbc.gridy = 0;
+		panelCenter.add(temp, gbc);
 
-		panelCenter.add(temperature,gbc);
-
-		
-
-		
-
-		 high = new JLabel("High: ");
-
-		gbc.gridx = 0;
-
+		highTemp = new JLabel("90");
+		gbc.gridx = 1;
 		gbc.gridy = 1;
+		panelCenter.add(highTemp, gbc);
 
-		panelCenter.add(high,gbc);
-
-		
-
-		 low = new JLabel("Low: ");
-
-		gbc.gridx = 0;
-
+		lowTemp = new JLabel("Zero");
+		gbc.gridx = 1;
 		gbc.gridy = 2;
+		panelCenter.add(lowTemp, gbc);
 
-		panelCenter.add(low,gbc);
-
-		
-
-		 humidity = new JLabel("Humidity: ");
-
-		gbc.gridx = 0;
-
+		humid = new JLabel("Super humid");
+		gbc.gridx = 1;
 		gbc.gridy = 3;
+		panelCenter.add(humid, gbc);
 
-		panelCenter.add(humidity,gbc);
-
-		
-
-		 chanceRain = new JLabel("Chance of Rain: ");
-
-		gbc.gridx = 0;
-
+		howMuchRain = new JLabel("Lots of Rain");
+		gbc.gridx = 1;
 		gbc.gridy = 4;
+		panelCenter.add(howMuchRain, gbc);
 
-		panelCenter.add(chanceRain, gbc);
-
-		
-
-		 cloudCover = new JLabel("Cloud Cover: ");
-
-		gbc.gridx = 0;
-
+		cloud = new JLabel("super cloudy ");
+		gbc.gridx = 1;
 		gbc.gridy = 5;
-
-		panelCenter.add(cloudCover, gbc);
-
-		
-
-		 windSpeed = new JLabel("Wind Speed: ");
-
-		gbc.gridx = 0;
-
-		gbc.gridy = 6;
-
-		panelCenter.add(windSpeed, gbc);
-
-		
-
-		 temp = new JLabel("Hot");
-
-		gbc.gridx = 1;
-
-		gbc.gridy = 0;
-
-		panelCenter.add(temp,gbc);
-
-		
-
-		 highTemp = new JLabel("90");
-
-		gbc.gridx = 1;
-
-		gbc.gridy = 1;
-
-		panelCenter.add(highTemp,gbc);
-
-		
-
-		 lowTemp = new JLabel("Zero");
-
-		gbc.gridx = 1;
-
-		gbc.gridy = 2;
-
-		panelCenter.add(lowTemp,gbc);
-
-		
-
-		 humid = new JLabel("Super humid");
-
-		gbc.gridx = 1;
-
-		gbc.gridy = 3;
-
-		panelCenter.add(humid,gbc);
-
-		
-
-		 howMuchRain = new JLabel("Lots of Rain");
-
-		gbc.gridx = 1;
-
-		gbc.gridy = 4;
-
-		panelCenter.add(howMuchRain,gbc);
-
-		
-
-		 cloud = new JLabel("super cloudy ");
-
-		gbc.gridx = 1;
-
-		gbc.gridy = 5;
-
 		panelCenter.add(cloud, gbc);
 
-		
-
-		 wind = new JLabel("no wind ");
-
+		wind = new JLabel("no wind ");
 		gbc.gridx = 1;
-
 		gbc.gridy = 6;
-
 		panelCenter.add(wind, gbc);
 
-		//------------------------------------------------------------------------
-
 		
-
-
-
-		
-
-		
-
-		
-
-		
-
-		
-
-	
-
-		
-
 		// create top panel and add title label to it
-
-		//**************************************************************************
-
 		panelNorth = new JPanel();
-
 		panelNorth.setBorder(borderRegion);
-
 		panelNorth.setPreferredSize(new Dimension(500, 50));
-
-		JLabel space2 = new JLabel("               ");
-
-		panelNorth.add(space2);
-
-		JLabel space3 = new JLabel("                         ");
-
-		panelNorth.add(space3);
-
-		JLabel lblNorth = new JLabel("SimpleWeather");
-
-		panelNorth.add(lblNorth);
-
-		JLabel space4 = new JLabel("                         ");
-
-		panelNorth.add(space4);
-
-		JLabel date = new JLabel("March 2nd");
-
-		panelNorth.add(date);
-
 		
+		panelNorth.add(new JLabel("               "));
+		panelNorth.add(new JLabel("                         "));
+		panelNorth.add(new JLabel("SimpleWeather"));
 
+		panelNorth.add(new JLabel("                         "));
+
+		date = new JLabel("March 2nd");
+		panelNorth.add(date);
 		contents.add(panelNorth, BorderLayout.NORTH);
 
-		//**************************************************************************
-
 		
-
-		
-
-		
-
-		
-
-		
-
-		// create bottom panel and add labels 
-
-		//**************************************************************************
-
+		// create bottom panel and add labels
 		panelSouth = new JPanel();
-
 		panelSouth.setBorder(borderRegion);
-
 		panelSouth.setPreferredSize(new Dimension(500, 100));
-
 		contents.add(panelSouth, BorderLayout.SOUTH);
-
 		
-
 		
-
-		
-
-		zip = new JLabel("Enter your zip code: ");
-
-		gbc.gridx = 0;
-
-		gbc.gridy = 0;
-
-		panelSouth.add(zip, gbc);
-
-		
+		placeLabel(panelSouth, gbc, "Enter your ZIP code", 0);
 
 		enterZip = new JTextField(5);
-
 		gbc.gridx = 1;
-
 		gbc.gridy = 0;
-
 		panelSouth.add(enterZip, gbc);
 
-		
-
-		
-
 		getWeather = new JButton("Get Weather Info");
-
 		gbc.gridx = 2;
-
 		gbc.gridy = 0;
-
 		panelSouth.add(getWeather, gbc);
 
-		
-
-		
-
-		 space = new JLabel("                             ");
-
 		gbc.gridx = 3;
-
 		gbc.gridy = 0;
-
-		panelSouth.add(space, gbc);
-
+		panelSouth.add(new JLabel("                             "), gbc);
 		
-
+		// see if can switch this to radio buttons or a switch.
+		// something more obvious for the user
 		usMetric = new JButton("US/Metric");
-
 		gbc.gridx = 4;
-
 		gbc.gridy = 0;
-
 		panelSouth.add(usMetric, gbc);
 
-	
-
-		
-
 		GetWeatherListener listener1 = new GetWeatherListener();
-
 		getWeather.addActionListener(listener1);
 
-		UsMetricListener listener2 = new UsMetricListener();
+		usMetric.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent event)
+			{
+				isMetric = !isMetric; // toggle between values
+			}
+		});
 
-		usMetric.addActionListener(listener2);
-
-		
-
-		//***************************************************************************
-
-		
-
-		
-
-		
-
-		
-
-		// create new jframe and put the entire contents panel inside.  
-
+		// create new jframe and put the entire contents panel inside.
 		// this is done to prevent GUI resizing
-
 		frame = new JFrame("SimpleWeather");
-
 		frame.add(contents);
-
 		frame.setResizable(false);
-
 		frame.pack();
-
 		frame.setVisible(true);
-
-		
-
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		
-
-		
-
-		
-
 	}
-
-	
 
 	// needed in order to set fonts
-
 	private void setFonts()
-
 	{
-
 		UIManager.put("Label.font", fontLabels);
-
-		
-
+	}
+	
+	private void placeLabel(JPanel pane, GridBagConstraints gbc, String id, int y) {
+		JLabel lb = new JLabel(id + ": ");
+		gbc.gridx = 0;
+		gbc.gridy = y;
+		pane.add(lb, gbc);
 	}
 
-	
-
-	
-
-	
-
-	
-
-	
-
-			
-
-		
-
-			
-
-			// listener class for the getWeather button
-
-			private class GetWeatherListener implements ActionListener
-
+	// listener class for the getWeather button
+	private class GetWeatherListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
+		{
+			// first, check for network connection
+			if (isNetworkUp())
 			{
-
-				public void actionPerformed (ActionEvent event)
-
+				try
 				{
-
+					String zip = validateZIP();
+					// TODO: get dates
 					
-
+					// TODO: get units
 					
-
+					// TODO: put in GetWeather object
 					
-
-
-
+					// TODO: refresh labels
 					
-
-				}	
-
+				} catch (InputMismatchException e)
+				{
+					// TODO: pop up an error dialog
+				}
 				
-
-			}
-
-			
-
-			
-
-			
-
-			// listener class for the usMetric button
-
-						private class UsMetricListener implements ActionListener
-
-						{
-
-							public void actionPerformed (ActionEvent event)
-
-							{
-
-								
-
-								
-
-								
-
-
-
-								
-
-							}	
-
-							
-
-						}
-
-			
-
-			
-
-						
-
-			// main method
-
-			public static void main(String[] args)
-
+			} else
 			{
-
-				SimpleWeather gui = new SimpleWeather();
-
-				gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-				
-
+				// TODO: pop up an error dialog
 			}
-
+		}
+	}
+	
+	private boolean isNetworkUp()
+	{
+		// google's faq should always be up, and it's 1/10 the size of google.com
+		// also, it doesn't redirect
+		try
+		{
+			URL url = new URL("https://www.google.com/intl/en/policies/faq/");
+			HttpURLConnection c = (HttpURLConnection) url.openConnection();
+			if (c.getResponseCode() == 200)
+				return true;
+			else
+				return false; // something needs to be done!! by the user, of course
 			
+		} catch (MalformedURLException e)
+		{
+			return false;
+		} catch (IOException e)
+		{
+			return false;
+		}
+	}
+	
+	// validates user input, and either returns it or throws an InputMismatchException
+	private String validateZIP()
+	{
+		// allow 5 or 9 digit ZIP codes, but truncate 9 digit ones
+		String zip = enterZip.getText();
+		if (!(zip.length()==5) || !(zip.length()==9) || !(zip.length()==10))
+		{
+			// 10 for zip codes like: 21229-3113
+			throw new InputMismatchException("ZIP code wrong length");
+		} else
+		{
+			try
+			{
+				Integer.parseInt(zip.substring(0, 5));
+				zip = zip.substring(0, 5);
+			} catch (NumberFormatException e)
+			{
+				// if this is triggered, then the first 5 digits of ZIP aren't numeric
+				throw new InputMismatchException("ZIP code not numeric");
+			}
+		}
+		return zip;
+	}
+	
+	// TODO: refreshes the labels to reflect the weather (and the date)
+	private void refreshLabels(GetWeather w)
+	{
+		
+	}
+	
+	// TODO: pops up an error dialog with the specified message
+	private void errorDialog(String msg)
+	{
+		
+	}
 
+	public static void main(String[] args)
+	{
+		SimpleWeather gui = new SimpleWeather();
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
-
-
-
-
-
